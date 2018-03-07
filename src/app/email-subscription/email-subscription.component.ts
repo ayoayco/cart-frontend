2cdf3f5af012913a1a2bc6f788fda337fdd61cf9
Ayo Ayco <ramon.aycojr@gmail.com>
2018-03-07 15:24:57 +0800

    add donate button and email subscription form

---
+++ src/app/email-subscription/email-subscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-subscription',
+  templateUrl: './email-subscription.component.html',
+  styleUrls: ['./email-subscription.component.css']
+})
+export class EmailSubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
